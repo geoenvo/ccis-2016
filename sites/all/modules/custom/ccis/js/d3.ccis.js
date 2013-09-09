0f4f12e6b3fea00bbabe9c76cd2483345c950461
tobiasb <tobias.baehr@reinblau.de>
2013-09-09 13:42:27 +0200

    Added example data to d3.ccis.js files

--- sites/all/modules/custom/ccis/js/d3.ccis.js
+++ sites/all/modules/custom/ccis/js/d3.ccis.js
@@ -1,6 +1,14 @@
 (function($) {
 Drupal.behaviors.ccis = {
   attach: function(d_context, settings) {
+  // CUSTOM CODING START
+    var dashboard_ID = '#ccis_dashboard';
+    var csvDrupalBase = settings.basePath + 'weather';
+    //var csvDrupalYear = csvDrupalBase + '/STATION/YYYY';
+    var csvFilesystemBase = settings.basePath + 'sites/default/files/csv';
+    //var csvFilesystemYear = csvFilesystemBase + '/FILENAME.csv';
+    // Coding d3.time.format("%d-%b-%y").parse; ...
+  // CUSTOM CODING END
   }
 }
 })(jQuery);
