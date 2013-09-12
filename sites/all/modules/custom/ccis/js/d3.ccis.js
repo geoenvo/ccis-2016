704aa07df3046e8de38ea0be13e08f8fdf4a016e
tobiasb <tobias.baehr@reinblau.de>
2013-09-12 18:06:18 +0200

    Prepared d3.ccis.js

--- sites/all/modules/custom/ccis/js/d3.ccis.js
+++ sites/all/modules/custom/ccis/js/d3.ccis.js
@@ -1,14 +1,15 @@
 (function($) {
 Drupal.behaviors.ccis = {
   attach: function(d_context, settings) {
-  // CUSTOM CODING START
-    var dashboard_ID = '#ccis_dashboard';
-    var csvDrupalBase = settings.basePath + 'weather';
-    //var csvDrupalYear = csvDrupalBase + '/STATION/YYYY';
-    var csvFilesystemBase = settings.basePath + 'sites/default/files/csv';
-    //var csvFilesystemYear = csvFilesystemBase + '/FILENAME.csv';
-    // Coding d3.time.format("%d-%b-%y").parse; ...
-  // CUSTOM CODING END
+    if (settings.ccis && settings.ccis.stations) {
+      //console.log(settings.ccis);
+      $.each(settings.ccis.stations, function(i, station) {
+        var selector = '#' + station.selector;
+        // JSON-Format
+        var path = station.path;
+        // Coding d3.time.format("%d-%b-%y").parse;
+        });
+    }
   }
 }
 })(jQuery);
